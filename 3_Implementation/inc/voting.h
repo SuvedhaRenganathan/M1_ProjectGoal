ece02bb9213da560fb6ba8f5591ad46372630c61
SuvedhaRenganathan <98879965+SuvedhaRenganathan@users.noreply.github.com>
2022-02-12 16:40:45 +0530

    Add files via upload

---
+++ 3_Implementation/inc/voting.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define total_no_of_parties
+#define party1 "Feather"
+#define party2 "Tree"
+#define party3 "Torchlight"
+#define party4 "Sun"
+#define party5 "Clock"
+#define party6 "Nota"
+
+int Countofvote1 = 0,
+    Countofvote2 = 0,
+    Countofvote3 = 0,
+    Countofvote4 = 0,
+    Countofvote5 = 0,
+    Notaofvotes  = 0,
+    Invalidvotes = 0;
