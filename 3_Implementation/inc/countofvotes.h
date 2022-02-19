a18d4a3b169e0a4af205d54a8a0a79a6c8fc7a63
SuvedhaRenganathan <98879965+SuvedhaRenganathan@users.noreply.github.com>
2022-02-19 21:43:52 +0530

    Delete countofvotes.h

--- 3_Implementation/inc/countofvotes.h
+++
@@ -1,9 +0,0 @@
-void Countofvotes(){
-    printf("\n \n  Voting data ");
-    printf("\n %s - %d", party1, Countofvote1);
-    printf("\n %s - %d", party2, Countofvote2);
-    printf("\n %s - %d", party3, Countofvote3);
-    printf("\n %s - %d", party4, Countofvote4);
-    printf("\n %s - %d", party5, Countofvote5);
-    printf("\n %s - %d", party6, Notaofvotes);
-    printf("\n %s - %d", "Spoiled Votes", Invalidvotes);
